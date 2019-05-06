d8828e0ddef11e23ec04daf4f6f54a28c3008190
ctritta <ctritta@timsoft.com>
2019-05-06 16:55:18 +0200

    timer fonctionnel

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,6 +1,10 @@
 import React, { Component, FormEvent } from 'react';
-
+import { ReduxState } from '../store';
 import Display from './Display';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { TimerActionTypes } from '../action-types/timer-action-types';
+import { startTimer, pauseTimer, stopTimer } from '../actions/timer-actions';
 
 export type UnitOfTime = 'hours' | 'minutes' | 'seconds';
 
@@ -14,9 +18,7 @@ export interface TimeState {
 }
 
 interface DispatchProps {
-  onStart: () => void;
-  onPause: () => void;
-  onStop: () => void;
+  //onStop: () => void;
 }
 
 interface StateProps {
@@ -25,7 +27,7 @@ interface StateProps {
 
 type Props = DispatchProps & StateProps;
 
-class Timer extends Component<{}, TimeState> {
+class Timer extends Component<Props, TimeState> {
   interval: any;
   constructor(props: any) {
     super(props);
@@ -35,13 +37,14 @@ class Timer extends Component<{}, TimeState> {
       seconds: '00',
       timeInterval: null,
       status: 'stopped',
+      //onStop: () => {}
     };
   }
 
   static getTotalMilliseconds = (state: TimeState) => {
     return (parseInt(state.hours) * 60 * 60
-      + parseInt(state.minutes) * 60
-      + parseInt(state.seconds)
+        + parseInt(state.minutes) * 60
+        + parseInt(state.seconds)
     ) * 1000;
   }
 
@@ -54,7 +57,6 @@ class Timer extends Component<{}, TimeState> {
   }
 
   setHours(hours: number) {
-    console.log(hours);
     if (hours < 0) {
       this.setState(() => ({ hours: '00' }));
     } else {
@@ -163,8 +165,7 @@ class Timer extends Component<{}, TimeState> {
       }
 
     }
-
-  };
+  }
 
   onBlur = (unitOfTime: UnitOfTime) => {
     this.onInputChange(unitOfTime);
@@ -212,4 +213,21 @@ class Timer extends Component<{}, TimeState> {
   }
 }
 
-export default Timer;
+const mapStateToProps = (state: ReduxState): StateProps => {
+  return {
+    status: state.status
+  };
+}
+
+const mapDispatchToProps = (dispatch: Dispatch<TimerActionTypes>, ownProps: {}): DispatchProps => {
+  return {
+    onStop: () =>{
+      dispatch(stopTimer())
+    }
+  };
+}
+
+export default connect<StateProps, DispatchProps, {}, ReduxState>(
+    mapStateToProps,
+    mapDispatchToProps
+)(Timer);

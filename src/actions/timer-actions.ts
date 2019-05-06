9eb9d4456d5eea0815bcb221f8b5a3859128aaed
nanaiori <eljaafarii.Najlaa@gmail.com>
2019-05-06 15:18:37 +0200

    second test

--- src/actions/timer-actions.ts
+++ src/actions/timer-actions.ts
@@ -1,19 +1,19 @@
 import { START_TIMER, PAUSE_TIMER, STOP_TIMER, TimerActionTypes } from '../action-types/timer-action-types';
 
-export const startTimer = (): TimerActionTypes => {
-  return {
-    type: START_TIMER
-  };
+export const startTimer = (): TimerActionTypes  => {
+    return {
+        type: START_TIMER
+    }
 }
 
 export const pauseTimer = (): TimerActionTypes => {
-  return {
-    type: PAUSE_TIMER
-  };
+    return {
+        type: PAUSE_TIMER
+    }
 }
 
 export const stopTimer = (): TimerActionTypes => {
-  return {
-    type: STOP_TIMER
-  };
+    return {
+        type: STOP_TIMER
+    }
 }

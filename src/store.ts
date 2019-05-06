b07417d1dedef3b6e3876899d6fdbae32c9217fe
Unknown <gabrielle.ripeau@gmail.com>
2019-05-06 16:05:56 +0200

    Third commit

--- src/store.ts
+++ src/store.ts
@@ -29,7 +29,8 @@ function reducer(state = defaultState, action: TimerActionTypes): ReduxState {
 }
 
 const store = createStore(
-  reducer
+  reducer,
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;

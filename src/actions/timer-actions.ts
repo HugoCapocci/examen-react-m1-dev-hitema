d7793cbe6b64c2badc600b94e9415fd5940a0267
Romain <borgromain@gmailcom>
2019-05-06 16:48:01 +0200

    commit exam v2

--- src/actions/timer-actions.ts
+++ src/actions/timer-actions.ts
@@ -1,2 +1,19 @@
-export const startTimer = ()  => {
+import {START_TIMER, STOP_TIMER, PAUSE_TIMER, TimerActionTypes} from '../action-types/timer-action-types';
+
+export const startTimer = (): TimerActionTypes  => {
+    return {
+        type: START_TIMER
+    }
+}
+
+export const pauseTimer = (): TimerActionTypes => {
+    return {
+        type: PAUSE_TIMER
+    }
+}
+
+export const stopTimer = (): TimerActionTypes => {
+    return {
+        type: STOP_TIMER
+    }
 }
